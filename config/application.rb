b7e5f15d49e09b480c17742d838e933fc667f32d
konpyu <konpyu@gmail.com>
2015-08-16 14:01:00 +0900

    set timelocal setting

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module ShibuyaAssets
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.time_zone = 'Tokyo'
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.paths << Rails.root.join("vendor", "bower_components")
     config.active_record.raise_in_transactional_callbacks = true
